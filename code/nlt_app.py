de406ce22df57461186e51ae62cf45fccb3f84e1
Cynthia Chiang <cynthia@Artemis.local>
2020-05-14 15:28:04 -0400

    Added code for filtering searched articles by topic

--- code/nlt_app.py
+++ code/nlt_app.py
@@ -13,7 +13,7 @@ df = pd.read_csv('articles_with_topics.csv')
 model = Doc2Vec.load("doc2vec_updated.model", mmap='r')
 
 # Function for searching most similar articles by similar keywords
-def most_sim_docs(word, n_articles=2, topn=3):
+def most_sim_docs(word, n_articles=5, topn=5):
     dfs = []
     sims = []
 
@@ -37,7 +37,7 @@ def most_sim_docs(word, n_articles=2, topn=3):
     new_df = pd.DataFrame(dfs)
     new_df['similarity_percentage'] = sims
     
-    return new_df
+    return new_df.drop_duplicates(subset='title')
 
 # User input
 user_input = st.text_input("Keyword:")
@@ -48,17 +48,17 @@ st.sidebar.markdown("[Topic Visualization](http://localhost:8888/view/projects/c
 
 # Search options
 sort_by = st.sidebar.selectbox("Order articles by:",
-                              ['None', 'Most Recent'])
+                              ['Most Relevant', 'Most Recent'])
 
 # Search by topic
 topic = st.sidebar.selectbox("Search by topic:",
                              ['', 'German', 'Cellular Expression', 'Heart and Lungs', 'Cell Studies', 
                               'At Risk Groups', 'Household Pets', 'Pre-existing Conditions', 
                               'Global Health', 'Hospital Studies', 'Respiratory Conditions', 
-                              'Severe Outbreaks', 'Nervous System', 'Research Studies', 
-                              'Symptoms and Treatment', 'Intestinal Reactions', 'Coronaviruses', 
-                              'French', 'Vaccines', 'Proteins', 'Tissues and Lesions', 'Italian', 
-                              'Detection', 'Spanish', 'Farm Animals', 'Host Infection'])
+                              'Severe Outbreaks','Nervous System', 'Research Studies', 
+                              'Symptoms and Treatment', 'Intestinal Reactions', 'Coronaviruses', 'French', 
+                              'Vaccines', 'Proteins', 'Tissues and Lesions', 'Italian', 'Detection',
+                              'Spanish', 'Farm Animals', 'Host Infection'])
 
 # Display results as link to article and article title
 if user_input == '':
@@ -71,7 +71,7 @@ else:
 # Display in certain order
 order = sort_by
 
-if order == 'None' and user_input in model.wv.vocab.keys():
+if order == 'Most Relevant' and user_input in model.wv.vocab.keys() and topic == '':
     for i in range(articles.shape[0]):
         st.markdown(f"[{articles.iloc[i]['title']}]({articles.iloc[i]['url']})")
         st.markdown(f"**Related topics**: {articles.iloc[i]['Keywords']}")
@@ -92,13 +92,24 @@ topic_list = ['German', 'Cellular Expression', 'Heart and Lungs', 'Cell Studies'
               'Household Pets', 'Pre-existing Conditions', 'Global Health', 'Hospital Studies', 
               'Respiratory Conditions','Severe Outbreaks', 'Nervous System', 'Research Studies', 
               'Symptoms and Treatment', 'Intestinal Reactions', 'Coronaviruses', 'French', 'Vaccines',
-              'Proteins', 'Tissues and Lesions', 'Italian', 'Detection', 'Spanish', 'Farm Animals', 
+              'Proteins', 'Tissues and Lesions', 'Italian', 'Detection', 'Spanish', 'Farm Animals',
               'Host Infection']
+
 topic_dict = {topic: float(i) for i, topic in enumerate(topic_list)}
 
-if topic_selected in topic_list:
+# Allows you to get all the articles on a certain topic only if is no keyword input
+if topic_selected in topic_list and user_input == '':
     article_by_topic = df.loc[df['Dominant_Topic'] == topic_dict[topic_selected], :]
     
     for i in range(article_by_topic.shape[0]):
         st.markdown(f"[{article_by_topic.iloc[i]['title']}]({article_by_topic.iloc[i]['url']})")
         st.markdown(f"{article_by_topic.iloc[i]['text_body'][:300]}...")
+
+# Filter articles search on keyword by topic
+if user_input in model.wv.vocab.keys() and topic_selected in topic_list:
+    articles_filtered_by_top = articles.loc[articles['Dominant_Topic'] == topic_dict[topic_selected], :]
+    
+    for i in range(articles_filtered_by_top.shape[0]):
+        st.markdown(f"[{articles_filtered_by_top.iloc[i]['title']}]({articles_filtered_by_top.iloc[i]['url']})")
+       # st.markdown(f"**Related topics**: {articles_filtered_by_top.iloc[i]['Keywords']}")
+        st.markdown(f"{articles_filtered_by_top.iloc[i]['text_body'][:300]}...")
